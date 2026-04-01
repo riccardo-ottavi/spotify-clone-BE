0e861f74175bbf43f68062a3e7d4a537b9fda877
riccardo-ottavi <ottavi.ps@libero.it>
2026-04-01 12:14:01 +0200

    inserisci logica index e show

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,11 +1,14 @@
+const songs = require('../data/songs')
+
 function index(req, res) {
-
-
+    res.json(songs);
 }
 
 function show(req, res) {
-    
-
+    const id = Number(req.params.id);
+    const song = songs.find(s => s.id === id);
+    if (!song) return res.status(404).json({ message: "Song not found" });
+    res.json(song);
 }
 
 module.exports = { index, show }
