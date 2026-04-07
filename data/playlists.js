3bf086265b8c60e43d6c898054abb3c1880f9870
riccardo-ottavi <ottavi.ps@libero.it>
2026-04-07 18:01:50 +0200

    fix: utilizzo playlist

--- data/playlists.js
+++ data/playlists.js
@@ -2,25 +2,25 @@ const playlists = [
   {
     id: 1,
     name: "Daily Mix 1",
-    image: "../sinister.jpeg",
+    image: "/images/sinister.jpeg",
     songIds: [1, 3, 5, 9, 18]
   },
   {
     id: 2,
     name: "Daily Mix 2",
-    image: "../split.jpeg",
+    image: "/images/split.jpeg",
     songIds: [7, 11, 13, 10, 17]
   },
   {
     id: 3,
     name: "Daily Mix 3",
-    image: "../mixdaily.jpeg",
+    image: "/images/mixdaily.jpeg",
     songIds: [6, 8, 15, 2, 24]
   },
   {
     id: 4,
     name: "Daily Mix 4",
-    image: "../radar.jpeg",
+    image: "/images/radar.jpeg",
     songIds: [12, 4, 16, 19, 20]
   }
   
